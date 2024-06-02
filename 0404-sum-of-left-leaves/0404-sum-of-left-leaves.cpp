4bd53c548b2156299baf0e001d27bfd0df5e838e
Harshit Kumar <96547256+hkumariitr@users.noreply.github.com>
2024-06-02 21:54:15 +0530

    Time: 7 ms (15.24%), Space: 14.5 MB (96.93%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -11,27 +11,22 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, int&sum,int &prev, bool isleft){
+    void dfs(TreeNode* root, int&sum, bool isleft){
         if(!root) return ;
-
-        dfs(root->left, sum,prev,true);
-
+        
         if(!root->left && !root->right && isleft){
-            cout<<"Adding sum for "<<root->val<<endl;
             sum += root->val;
         }
-        if(root->right) prev = root->right->val;
-        cout<<"prev is "<<prev<<endl;
-        
-        dfs(root->right, sum,prev,false);
+
+        dfs(root->left, sum,true);        
+        dfs(root->right, sum,false);
     }
     int sumOfLeftLeaves(TreeNode* root) {
         int sum =0;
-        int prev = INT_MAX;
         if(!root->left && !root->right){
             return 0;
         }
-        dfs(root,sum,prev,true);
+        dfs(root,sum,false);
 
         return sum;
     }
