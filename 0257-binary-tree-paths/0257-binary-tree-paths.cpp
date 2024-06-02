466dd55efa7381cb71046e94bdc6e599fa8befe6
Harshit Kumar <96547256+hkumariitr@users.noreply.github.com>
2024-06-02 18:00:00 +0530

    Time: 9 ms (6.21%), Space: 16.2 MB (63.74%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -14,22 +14,20 @@ public:
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> res;
         if (!root) return res; // Base case
-        string path = to_string(root->val);
-        if(!root->left && !root->right){
-            res.push_back(path);
-            return res;
-        }
-        dfs(root->left, path, res);
-        dfs(root->right, path, res);
-        
+
+        dfs(root, "", res);
         return res;
     }
 
     void dfs(TreeNode* node, string path, vector<string>& res) {
         if (!node) return;
 
-        path += "->" + to_string(node->val);
-        
+        if (path.empty()) {
+            path += to_string(node->val);
+        } else {
+            path += "->" + to_string(node->val);
+        }
+
         if (!node->left && !node->right) {
             res.push_back(path); // Found a leaf node, add the current path to the result
             return;
