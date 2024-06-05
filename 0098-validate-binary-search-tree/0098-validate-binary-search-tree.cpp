2e1c7ba4af52cadc8a5ddfe66cda1a7993b90693
Harshit Kumar <96547256+hkumariitr@users.noreply.github.com>
2024-06-05 22:14:03 +0530

    Time: 10 ms (41.21%), Space: 20.1 MB (60.85%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -10,19 +10,33 @@
  * };
  */
 class Solution {
+    TreeNode* prev;
 public:
-    bool check(TreeNode* root, long long lower, long long upper) {
-    if (root == NULL) {
-        return true;
+    bool inorder(TreeNode* root){
+        if(!root) return true;
+
+        bool l = inorder(root->left);
+
+        if(prev != NULL && root->val <=prev->val){
+            return false;
+        }
+        prev = root;
+        bool r = inorder(root->right);
+        return l && r;
     }
-    if (root->val <= lower || root->val >= upper) {
-        return false;
-    }
-    return check(root->left, lower, root->val) && check(root->right, root->val, upper);
-}
+//     bool check(TreeNode* root, long long lower, long long upper) {
+//     if (root == NULL) {
+//         return true;
+//     }
+//     if (root->val <= lower || root->val >= upper) {
+//         return false;
+//     }
+//     return check(root->left, lower, root->val) && check(root->right, root->val, upper);
+// }
 
 bool isValidBST(TreeNode* root) {
-    return check(root, LONG_LONG_MIN, LONG_LONG_MAX);
+    prev = NULL;
+    return inorder(root);
 }
 
 };
