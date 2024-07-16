30d17745624ada42640e337fe60b9726d0eed097
Harshit Kumar <96547256+hkumariitr@users.noreply.github.com>
2024-07-17 01:07:52 +0530

    Time: 3 ms (97.58%), Space: 10.4 MB (19.44%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -8,17 +8,26 @@
  */
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
-        set<ListNode*> vis;
-        ListNode* temp = head;
-        while(temp){
-            if(vis.find(temp)!=vis.end()){
-                return temp;
-            }else{
-                vis.insert(temp);
-                temp = temp->next;
-            }
+    ListNode *getSlow(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow==fast) return slow;
+
         }
         return NULL;
+    }
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* slow = getSlow(head);
+        if(!slow) return NULL;
+        while(slow != head){
+            slow = slow->next;
+            head = head->next;
+        }
+        return head;
     }
 };
