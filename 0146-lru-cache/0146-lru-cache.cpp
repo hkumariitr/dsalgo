248d160ecbbd93064c2df3eeb1f4ac90849623dc
Harshit Kumar <96547256+hkumariitr@users.noreply.github.com>
2024-07-18 20:19:20 +0530

    Time: 373 ms (33.37%), Space: 183.6 MB (10.88%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,41 +1,70 @@
 class LRUCache {
-public:
-    list<int> dll; //it contains the key
-    map<int, pair<list<int>::iterator, int>> cache; //key->(list_node, value)
-    int capacity;
-    
-    LRUCache(int capacity) {
-        this->capacity = capacity;
+  public:
+    class node {
+      public:
+        int key;
+      int val;
+      node * next;
+      node * prev;
+      node(int _key, int _val) {
+        key = _key;
+        val = _val;
+      }
+    };
+
+  node * head = new node(-1, -1);
+  node * tail = new node(-1, -1);
+
+  int cap;
+  unordered_map < int, node * > m;
+
+  LRUCache(int capacity) {
+    cap = capacity;
+    head -> next = tail;
+    tail -> prev = head;
+  }
+
+  void addnode(node * newnode) {
+    node * temp = head -> next;
+    newnode -> next = temp;
+    newnode -> prev = head;
+    head -> next = newnode;
+    temp -> prev = newnode;
+  }
+
+  void deletenode(node * delnode) {
+    node * delprev = delnode -> prev;
+    node * delnext = delnode -> next;
+    delprev -> next = delnext;
+    delnext -> prev = delprev;
+  }
+
+  int get(int key_) {
+    if (m.find(key_) != m.end()) {
+      node * resnode = m[key_];
+      int res = resnode -> val;
+      m.erase(key_);
+      deletenode(resnode);
+      addnode(resnode);
+      m[key_] = head -> next;
+      return res;
     }
-    
-    void makeMostRecentlyUsed(int key) {
-        dll.erase(cache[key].first);
-        dll.push_front(key);
-        cache[key].first = dll.begin();
+
+    return -1;
+  }
+
+  void put(int key_, int value) {
+    if (m.find(key_) != m.end()) {
+      node * existingnode = m[key_];
+      m.erase(key_);
+      deletenode(existingnode);
     }
-    
-    int get(int key) {
-        if(!cache.count(key))
-            return -1;
-        
-        makeMostRecentlyUsed(key);
-        return cache[key].second;
+    if (m.size() == cap) {
+      m.erase(tail -> prev -> key);
+      deletenode(tail -> prev);
     }
-    
-    void put(int key, int value) {
-        if(cache.count(key)) {
-            cache[key].second = value;
-            makeMostRecentlyUsed(key);
-        } else {
-            dll.push_front(key);
-            cache[key] = {dll.begin(), value};
-            capacity--;
-        }
-        
-        if(capacity < 0) {
-            cache.erase(dll.back());
-            dll.pop_back();
-            capacity++;
-        }
-    }
+
+    addnode(new node(key_, value));
+    m[key_] = head -> next;
+  }
 };
