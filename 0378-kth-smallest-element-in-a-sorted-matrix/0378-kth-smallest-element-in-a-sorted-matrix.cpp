c34e737b8c96483000256d9a1f8b8d202fac8bb3
Harshit Kumar <96547256+hkumariitr@users.noreply.github.com>
2024-07-25 01:22:50 +0530

    Time: 73 ms (5.42%), Space: 23.9 MB (7.84%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -2,36 +2,37 @@ class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         if (matrix.empty() || k < 1) return -1; // Return -1 for invalid input
-
+        using mpair = pair<int,pair<int,int>>;
         // Min-heap using priority_queue
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> heap;
+        priority_queue<mpair, vector<mpair>, greater<mpair>> heap;
         set<pair<int, int>> s;
 
         // Add the first element
-        heap.push({matrix[0][0], 0, 0});
+        heap.push({matrix[0][0], {0, 0}});
         s.insert({0, 0});
 
         while (k > 1) {
-            vector<int> top = heap.top();
+            auto indices = heap.top().second;
             heap.pop();
-            int row = top[1];
-            int col = top[2];
+            
+            int row = indices.first;
+            int col = indices.second;
 
             // Add the next element in the row
             if (col + 1 < matrix[0].size() && s.find({row, col + 1}) == s.end()) {
-                heap.push({matrix[row][col + 1], row, col + 1});
+                heap.push({matrix[row][col + 1], {row, col + 1}});
                 s.insert({row, col + 1});
             }
 
             // Add the next element in the column
             if (row + 1 < matrix.size() && s.find({row + 1, col}) == s.end()) {
-                heap.push({matrix[row + 1][col], row + 1, col});
+                heap.push({matrix[row + 1][col], {row + 1, col}});
                 s.insert({row + 1, col});
             }
 
             k--;
         }
 
-        return heap.top()[0];
+        return heap.top().first;
     }
 };
