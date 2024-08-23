5ae896a71a5cda144a6bb0d6eefa87f9962a4c6c
Harshit Kumar <96547256+hkumariitr@users.noreply.github.com>
2024-08-23 13:30:05 +0530

    Time: 910 ms (19.3%), Space: 178.8 MB (17.48%) - LeetHub

--- 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.cpp
+++ 0802-k-th-smallest-prime-fraction/0802-k-th-smallest-prime-fraction.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
+    typedef vector<double> V;
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        int cnt=0;
-        pair<double,pair<int,int>> mini;
-        using pi = pair<double,pair<int,int>>;
-        priority_queue<pi,vector<pi>,greater<pi>> pq;
         int n = arr.size();
-        for(int j=n-1;j>=0;j--){
-            for(int i=0;i<j;i++){
-                double ans = (arr[i]*1.0)/arr[j];
-                pq.push({ans,{i,j}});
-            }
-            mini = pq.top();
-            cnt++;
-            pq.pop();
-            if(cnt==k) break;
+        priority_queue<V, vector<V>, greater<V>> pq; // min-heap (fraction, i,
+                                                     // j)
+        for (int i = 0; i < n; i++) {
+            // denominator = arr[n-1]; //largest
+            pq.push({1.0 * arr[i] / arr[n - 1], (double)i, (double)n - 1});
         }
-        while(cnt!=k){
-            mini = pq.top();
-            cnt++;
+        int smallest = 1; // first smallest
+
+        while (smallest < k) {
+            V vec = pq.top();
             pq.pop();
+            int i = vec[1];     // index
+            int j = vec[2] - 1; // index
+            pq.push({1.0 * arr[i] / arr[j], (double)i, (double)j});
+            smallest++;
         }
-        return {arr[mini.second.first], arr[mini.second.second]};
+
+        V vec = pq.top();
+        int i = vec[1];
+        int j = vec[2];
+
+        return {arr[i], arr[j]};
     }
 };
